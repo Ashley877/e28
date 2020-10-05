76aa3786de4950f2af34675f3af4a579062ee69b
Ashley877 <ash877@g.harvard.edu>
2020-10-05 13:10:37 -0400

    update to code

--- p1/script.js
+++ p1/script.js
@@ -1,7 +1,61 @@
 let app = new Vue({
     el: '#app',
     data: {
-        'item': null,
-        'items': [] // New
+        'player': [],
+        'computer': [] 
+
     }
-});
+}),
+
+function computer () {
+    var computer = Math.random();
+    if (computer < 0.34) {
+        computerPick = "Rock";
+    } else if (computer < 0.67) {
+        computerPick = "Paper";
+    } else {
+        computerPick = "Scissors";
+}
+}
+
+
+
+function winner() {
+  var userPick = this.id;
+  console.log("You: " + userPick);
+  console.log("Computer: " + computerPick);
+  var result = game(userPick, computerPick);
+  console.log(game(userPick, computerPick));
+  function game(pickU, pickC) {
+    if (pickU === pickC) {
+      return "It's a tie!";
+    } else if (pickU === "Rock") {
+      if (pickC === "Scissors") {
+        return "You Win!";
+      } else {
+        return "You Lose!";
+      }
+    } else if (pickU === "Paper") {
+      if (pickC === "Rock") {
+        return "You Win!";
+      } else {
+        return "You Lose!";
+      }
+    } else if (pickU === "Scissors") {
+      if (pickC === "Paper") {
+        return "You Win!";
+      } else {
+        return "You Lose!";
+      }
+    }
+  }
+  alert(
+    "You picked: " +
+      userPick +
+      "    " +
+      " Computer picked: " +
+      computerPick +
+      "   " +
+      result
+  );
+}
